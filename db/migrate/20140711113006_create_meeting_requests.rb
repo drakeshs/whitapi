e90ccb66b59fa51714fc61dd57fedfdc2cf26a76
panzer <panzer@panzer-desktop.(none)>
2014-07-28 23:36:20 +0530

    changed value to bigint

--- db/migrate/20140711113006_create_meeting_requests.rb
+++ db/migrate/20140711113006_create_meeting_requests.rb
@@ -3,7 +3,7 @@ class CreateMeetingRequests < ActiveRecord::Migration
     create_table :meeting_requests do |t|
       t.string   :name
       t.string   :email
-      t.bigint  :phone_number, :limit => 10
+      t.string  :phone_number, :limit => 10
       t.integer  :accountant_id
       t.datetime :appointment_time
       t.text     :purpose_of_meeting
