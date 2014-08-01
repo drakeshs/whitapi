4e3ee4c749409ccfc3d9c03a6c1ceb66a4c7db42
panzer <panzer@panzer-desktop.(none)>
2014-08-01 14:44:37 +0530

    added exclaim

--- app/controllers/api/v1/meeting_request_controller.rb
+++ app/controllers/api/v1/meeting_request_controller.rb
@@ -1,8 +1,7 @@
 module Api
 	module V1
 		class MeetingRequestController < Api::ApiController
-		
-			def accountants
+		  def accountants
 				@accountants = Accountant.all
 				respond_to do |format|
 					format.json {render :json => {:response_code => response.code, :accountants => @accountants}}
@@ -11,9 +10,10 @@ module Api
 
 			def send_meeting_request
 	  		ids = params[:meeting_details]["accountant_id"]
+        p params[:meeting_details]
   			@meeting_request = MeetingRequest.new(params[:meeting_details])
   			@meeting_request.accountant_id = Accountant.where(name: "#{ids}").first.id
-	      if @meeting_request.save  
+	      if @meeting_request.save! 
 					render json: {status: :created}
 	      else  
 					render json: @meeting_request.errors, status: :unprocessable_entity
