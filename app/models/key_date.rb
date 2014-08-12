124b05b86b904f32c2a70329b03f91d56a0bc43d
panzer <panzer@panzer-desktop.(none)>
2014-08-12 23:56:22 +0530

    changed the where condition used pgs extract method

--- app/models/key_date.rb
+++ app/models/key_date.rb
@@ -1,5 +1,7 @@
 class KeyDate < ActiveRecord::Base
   attr_accessible :month, :obligation, :tax_date, :title
-  scope :keydates, ->(year,month){where("MONTH(tax_date) = ? and YEAR(tax_date) = ?", month,year)}
+  scope :keydates, ->(year,month){where('extract(year from tax_date) = ? and extract(month from tax_date) = ?', year,month)}
 end
 
+
+# where('extract(year from tax_date) = ? and extract(month from tax_date) = ?', month,year)
