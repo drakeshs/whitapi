37c9b025cf6e75b7cd028113439677ce9683baf7
RAKESH SHARMA <drakeshs@gmail.com>
2018-11-19 15:10:34 -0700

    Update api_controller.rb

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -11,17 +11,3 @@ class Api::ApiController < ActionController::Metal
 	append_view_path "#{Rails.root}/app/views"
 	respond_to :json
 end
- 
-
-# class Api::BaseController < ActionController::Metal
-# include ActionController::UrlFor
-# include ActionController::Rendering
-# include ActionController::Renderers::All
-# include ActionController::ConditionalGet
-# include ActionController::MimeResponds
-# include ActionController::Rescue
-# include ActionController::Instrumentation
-# include Rails.application.routes.url_helpers
-# append_view_path "#{Rails.root}/app/views"
-# respond_to :json
-# end
